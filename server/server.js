22d41b56dc27a54760064fae81bb3b6c8ca0fd49
guptaDeepak <Deepakkumar346688@gmail.com>
2026-02-22 21:55:51 +0530

    Error Solve

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://wanderai.vercel.app' // ← apna Vercel URL
+    'https://wander-ai-travle-planner.vercel.app' // ← apna Vercel URL
   ],
   credentials: true
 }));
